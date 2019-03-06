2533b9edd981d1da728b42697c0015b311e7d76b
Dane Van Domelen <vandomed@gmail.com>
2019-03-06 10:02:16 -0500

    Nothing really

--- R/pooling.R
+++ R/pooling.R
@@ -10,7 +10,7 @@
 #' Package: \tab pooling \cr
 #' Type: \tab Package \cr
 #' Version: \tab 1.1.2 \cr
-#' Date: \tab 2019-03-01 \cr
+#' Date: \tab 2019-03-06 \cr
 #' License: \tab GPL-3 \cr
 #' }
 #'
